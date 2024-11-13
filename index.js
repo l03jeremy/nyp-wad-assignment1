77665d544004c057b48489ab1dd280c9500107ed
l03jeremy <lee03jeremy@gmail.com>
2024-11-14 00:23:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const rental = require('./Jeremy_CarRental.js');
 
-//console.log(rental.checkBookings("SDP8160L"));
-rental.addCar("Honda", "NBox", 5, 660, "t");
-//rental.searchCar({seats: 5});
+// Add a car
 
+rental.addCar("Honda", "N-Box", 5, 660, "SGN9320K");
 
-console.log(rental.rentCar("t", 1, Date.now(), Date.now() + 7200000));
-console.log(rental.rentCar("t", 2, Date.now(), Date.now() + 7200000));
-console.log(rental.rental)
-//console.log(rental.endRental(3));
+// Start a rental
+
+console.log(rental.rentCar("SGN9320K", 1, Date.now(), Date.now() + 7200000)); // The endDateTime is set 2 hours from now
+
+// When a user tries to book a car that is still rented under another user.
+
+console.log(rental.rentCar("SGN9320K", 2, Date.now(), Date.now() + 7200000));
+
+// End the rental
+
+console.log(rental.endRental(3));
+
+// Booking a car without providing startDateTime (the rental will start immediately) and/or endDateTime (the rental will be charged at a higher rate)
+
+console.log(rental.rentCar("SFK3390A", 1));
+
+// Search for a car with desired requirements
+
+console.log(rental.searchCar({seats: 5}));
+console.log(rental.searchCar({make: "Toyota"}));
+
+// Check pervious bookings made with a specific car, ongoing or not
+
+console.log(rental.checkBookings("SGN9320K"));
