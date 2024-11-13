1e5ede58c52ef956e63eccbb328ac6fd4f61d05c
l03jeremy <lee03jeremy@gmail.com>
2024-11-13 23:47:41 +0800

    i think it works now (i think)
    its only a possibility

--- Jeremy_CarRental.js
+++ Jeremy_CarRental.js
@@ -56,44 +56,50 @@ module.exports = {
             startDateTime: 1731249100000,
             endDateTime: 1731340800000,
             vehicleID: "SDQ320P",
-            ended: true,
+            ended: false,
         },
     ],
-    // Explain what function A does
-    addCar(make, model, seats, cap) {
-        const id = Math.max(...vehicles.map(vehicle => vehicle.id)) + 1;
-        this.vehicles.push({id, make, model, seats, cap});
+    addCar(make, model, seats, cap, regno) {
+        const id = Math.max(...this.vehicles.map(vehicle => vehicle.id))+1;
+        this.vehicles.push({id, make, model, seats, cap, regno});
     },
-    // Explain what function B does
     register(username, name, age, phone) {
-        if(username, name, age, phone) {
-
+        if(!username || !name || !age || !phone) {
+            return `All fields required`;
         }
         const id = Math.max(...user.map(u => u.id)) + 1;
         this.user.push({id, username, name, age, phone});
         return `Thank you, ${name}, you have successfully registered.`;
     },
-    rentCar(vehicleID, customerID, startDateTime = null, endDateTime = null) {
+    rentCar(regno, customerID, startDateTime = null, endDateTime = null) {
+        if(!customerID || !regno) return `Customer ID and car register number is required.`;
+        var vehicleID = this.vehicles.find(v => v.regno == regno)?.id;
+        if(!vehicleID) return `No vehicles found matching '${regno}'`;
+
+        const id = Math.max(...this.rental.map(r => r.id)) + 1;
         var date = new Date();
-        const id = Math.max(...rental.map(r => r.id)) + 1;
         var bookdate = startDateTime || date.getTime();
-        if(this.rental.find(r => r.vehicleID == vehicleID) !== null || this.rental.find(r => r.vehicleID == vehicleID).ended) {
+        var cardetail = this.vehicles.find(v => v.id == vehicleID);
+        if((endDateTime !== null && endDateTime < date.getTime()) || (startDateTime !== null && startDateTime < date.getTime())) return `You cannot end rental in the past`;
+        if(!this.rental.find(r => r.vehicleID == regno && !r.ended)) {
+            
             this.rental.push({
                 id, 
                 customerID, 
                 startDateTime: bookdate, 
                 endDateTime, 
-                vehicleID, 
+                vehicleID: regno, 
                 ended: false,
             });
-            return `Vehicle rental started. Your car is ${this.vehicles.find(v => v.id == Number(id)).regno}. \n
+            return `Vehicle rental started. Your car is ${cardetail.regno}. \n
             Booking details: \n
+            Booking ID: ${id}, \n
             User: ${this.user.find(u => u.id == customerID).name}, \n
-            Car: ${this.rental.find(r => r.vehicleID == vehicleID).regno}, \n
+            Car: ${cardetail.regno}, \n
             Start Time: ${date.toLocaleString()}, \n
-            ${endDateTime ? `End Time: ${endDateTime}, \n` : "\nNo end time provided, you are being charged the normal rate."}`;
+            ${endDateTime ? `End Time: ${new Date(endDateTime).toLocaleString()}, \n` : "No end time provided, you are being charged the normal rate."}\n`;
         } else {
-            return `Vehicle rental failed. Car ${this.vehicles.find(v => v.id == Number(id)).regno} has already been booked.`;
+            return `Vehicle rental failed. Car ${cardetail.regno} has already been booked.`;
         }
     },
     endRental(i) {
@@ -106,17 +112,15 @@ module.exports = {
 
         var fees;
         if(reservation.endDateTime !== null) {
-            fees = Math.round(((enddate - reservation.startDateTime) / (3600000)) * hourlyrate * 100) / 100;  // If the user has stated their end period before, calculate the hourly rate;
+            fees = Math.round(((reservation.endDateTime - reservation.startDateTime) / (3600000)) * hourlyrate * 100) / 100;  // If the user has stated their end period before, calculate the hourly rate;
             if(enddate > reservation.endDateTime && (enddate - reservation.endDateTime) > 900000) {  // Give the user a 15 minute grace period to return their vehicles (like GetGo)
                 var overdue = (enddate - reservation.endDateTime) / 60000;
                 fees = fees + (overdue * overdueratebymin);  // Surcharge the user for dishonesty if they did not return the car after their stated rental period.
             }
         } else {
-            if(enddate - reservation.startDateTime < 3600000) {
-                fees ;
-            }
-            fees = (reservation.startDateTime - reservation.endDateTime) / (1000 * 60 * 60) * normalrate;  // Find out the hours rented by the user and calculate the fees by the normal rate
+            fees = Math.ceil((enddate - reservation.startDateTime) / (1000 * 60 * 60)) * normalrate;  // Find out the hours rented by the user and calculate the fees by the normal rate, the minimum hours rented is 1 hour
         }
+        reservation.endDateTime = enddate;
         reservation.ended = true;
         return `Your rental has ended. Outstanding fees: ${fees}`;
     },
@@ -140,17 +144,19 @@ module.exports = {
             const matchesModel = detail.model ? v.model.toLowerCase() === detail.model.toLowerCase() : true;
             const matchesCap = detail.cap ? v.cap === detail.cap : true;
             const matchesSeats = detail.seats ? v.seats === detail.seats : true;
-            const matchesRegno = detail.regno ? v.regno.toLowerCase() === detail.regno.toLowerCase() : true; // Checks if 
+            const matchesRegno = detail.regno ? v.regno.toLowerCase() === detail.regno.toLowerCase() : true; // Only check the vehicles array based on what is provided in the detail argument.
     
             return matchesMake && matchesModel && matchesCap && matchesSeats && matchesRegno;
         });
 
-        console.log(cars);
+        return cars;
     },
     checkBookings(regno) {
         // In case the car is damaged or vandalized, the admin can look through previous bookings to find the culprit.
         var vehicle = this.vehicles.find(v => v.regno == regno);
-        var reservations = this.reservations.find(r => r.vehicleID == vehicle.id);
-
+        if(!vehicle) return `No vehicles found`;
+        var reservations = this.rental.filter(r => r.vehicleID == vehicle.regno);
+        if(reservations.length === 0) return `No one has booked this car`; // Since .filter() always returns an array, we check if the length is 0;
+        return reservations;
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const rental = require('./Jeremy_CarRental.js');
 
-//rental.addCar({"2", "vios"})
-//rental.addCustomer
-//console.log(rental.searchCar(1));
-rental.searchCar({seats: 7})
+//console.log(rental.checkBookings("SDP8160L"));
+rental.addCar("Honda", "NBox", 5, 660, "t");
+//rental.searchCar({seats: 5});
+
+
+console.log(rental.rentCar("t", 1, Date.now(), Date.now() + 7200000));
+console.log(rental.rentCar("t", 2, Date.now(), Date.now() + 7200000));
+console.log(rental.rental)
+//console.log(rental.endRental(3));
